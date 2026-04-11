6a32598e94f6ab4057916ac54feba2185c5f52d7
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-12 01:35:22 +0600

    Add ruting just

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h2>This is dashBroad</h2>
+        </div>
+    );
+};
+
+export default DashboardPage;
---
+++ src/app/installation/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstallationPage = () => {
+    return (
+        <div>
+            <h2>This is installation page</h2>
+        </div>
+    );
+};
+
+export default InstallationPage;
